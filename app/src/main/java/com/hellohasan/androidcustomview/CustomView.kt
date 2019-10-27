3e0eb94ae7df3fcc9a61965ca06b60d244912b90
Hasan Abdullah <hasan.cse91@gmail.com>
2019-10-27 13:21:07 +0600

    custom view code working now

--- app/src/main/java/com/hellohasan/androidcustomview/CustomView.kt
+++ app/src/main/java/com/hellohasan/androidcustomview/CustomView.kt
@@ -3,6 +3,8 @@ package com.hellohasan.androidcustomview
 import androidx.constraintlayout.widget.ConstraintLayout
 
 import android.content.Context
+import android.graphics.drawable.AnimatedImageDrawable
+import android.graphics.drawable.Drawable
 import android.widget.TextView
 import android.util.AttributeSet
 import android.view.LayoutInflater
@@ -10,43 +12,76 @@ import android.view.View
 import android.widget.ImageView
 import androidx.annotation.Nullable
 import kotlinx.android.synthetic.main.custom_view.view.*
-
+import java.lang.Exception
 
 class CustomView(context: Context, @Nullable attrs: AttributeSet) : ConstraintLayout(context, attrs) {
 
     private var view : View = LayoutInflater.from(context).inflate(R.layout.custom_view, this, true)
+    private var imageView: ImageView
+    private var titleTextView: TextView
+    private var subtitleTextView: TextView
+    private var imageDrawable : Drawable?
+    private var title: String?
+    private var subtitle: String?
 
     init {
+        imageView = view.imageView as ImageView
+        titleTextView = view.titleTextView as TextView
+        subtitleTextView = view.subtitleTextView as TextView
 
-        val title: String?
-        val subtitle: String?
-        val a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.CustomView, 0, 0)
+        val typedArray = context.theme.obtainStyledAttributes(attrs, R.styleable.CustomView, 0, 0)
 
         try {
-            title = a.getString(R.styleable.CustomView_customViewTitle)
-            subtitle = a.getString(R.styleable.CustomView_customViewSubtitle)
-        } finally {
-            a.recycle()
+            imageDrawable = typedArray.getDrawable(R.styleable.CustomView_setImageDrawable)
+            title = typedArray.getString(R.styleable.CustomView_setTitle)
+            subtitle = typedArray.getString(R.styleable.CustomView_setSubTitle)
+
+            imageView.setImageDrawable(imageDrawable)
+            titleTextView.text = title
+            subtitleTextView.text = subtitle
+        }
+        finally {
+            typedArray.recycle()
         }
 
-        // Throw an exception if required attributes are not set
+        /** Uncomment below line if all of your attribute fields are required.
+         * Throw an exception if required attributes are not set. It will caused Run Time Exception.
+         * In this sample project we assume that, no attributes are mandatory
+         * *
+         */
+        /*
+        if (imageDrawable == null)
+            throw RuntimeException("No image drawable provided")
         if (title == null) {
             throw RuntimeException("No title provided")
         }
         if (subtitle == null) {
             throw RuntimeException("No subtitle provided")
-        }
-
-        initView(title, subtitle)
+        }*/
     }
 
-    // Setup views
-    private fun initView(title: String, subtitle: String) {
-        val imageView = view.imageView as ImageView
-        val titleView = view.titleTextView as TextView
-        val subtitleView = view.subtitleTextView as TextView
+    /**
+     * Below getter-setter will work, if we need to access the attributes programmatically
+     */
 
-        titleView.text = title
-        subtitleView.text = subtitle
+    fun setImageDrawable(drawable: Drawable?) {
+        imageView.setImageDrawable(drawable)
+    }
+    fun getImageDrawable() : Drawable? {
+        return imageDrawable
+    }
+
+    fun setTitle(text: String?) {
+        titleTextView.text = text
+    }
+    fun getTitle() : String? {
+        return title
+    }
+
+    fun setSubtitle(text: String?) {
+        subtitleTextView.text = text
+    }
+    fun getSubtitle() : String? {
+        return subtitle
     }
 }
--- app/src/main/java/com/hellohasan/androidcustomview/MainActivity.kt
+++ app/src/main/java/com/hellohasan/androidcustomview/MainActivity.kt
@@ -2,11 +2,17 @@ package com.hellohasan.androidcustomview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // image drawable and title are already set from xml.
+        // I want to set subtitle programmatically. you can set title and image drawable from here
+        sunrise_custom_view.setSubtitle("5:31 AM") // sunrise time set as subtitle
+        sunset_custom_view.setSubtitle("5:01 PM") // subset time set as subtitle
     }
 }
